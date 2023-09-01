e35773d187594885d057571301910bc0fcb57976
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2023-09-01 09:40:12 +0200

    Add project which uses library

---
+++ usage/src/com/github/vladrassokhin/usage/Main.java
@@ -0,0 +1,9 @@
+package com.github.vladrassokhin.usage;
+
+import com.github.vladrassokhin.library.LibraryClass;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new LibraryClass("Hello").getInput());
+    }
+}
